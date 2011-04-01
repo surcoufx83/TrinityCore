1fa6677f6b68b0288a7c934451e49ba5bd7c2f5c
treak <stg90@gmx.de>
2011-04-01 22:44:05 +0200

    DB/quest_template - Fix 	The Tome of Divinity (part7)

--- sql/updates/world/2011_03_29_lol_world_quest_template_1768.sql
+++ sql/updates/world/2011_03_29_lol_world_quest_template_1768.sql
@@ -5,5 +5,5 @@ INSERT INTO `creature` (`guid`, `id`, `map`, `spawnMask`, `phaseMask`, `modelid`
 (81163, 6172, 0, 1, 1, 0, 0, -9129.59, -984.313, 76.1811, 2.05949, 180, 0, 0, 186, 191, 0, 0, 0, 0, 0);
 -- quest_template Changes 
 UPDATE `quest_template` SET `ReqCreatureOrGOId1` = 6172, `ReqCreatureOrGOCount1` = 1, `ReqSpellCast1` = 8593 WHERE `entry` = 1786; 
--- creature_template
+-- creature_template - disable move
 UPDATE `creature_template` SET `unit_flags` = `unit_flags` | 4 WHERE `entry` = 6172;
