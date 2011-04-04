1a44c853d2b401c5d1f3e4c7b0126c002447861e
treak <stg90@gmx.de>
2011-04-04 10:29:49 +0200

    DB/Quests Fix Quest 11648 The Art of Persuasion, thanks to treak
    //revised

--- sql/updates/world/2011_03_24_8_lol_world_gameobject.sql
+++ sql/updates/world/2011_03_24_8_lol_world_gameobject.sql
@@ -3,7 +3,7 @@
 DELETE FROM `gameobject` WHERE `id` = 175382;
 INSERT INTO `gameobject` (id, map, spawnMask, phaseMask, position_x, position_y, position_z, orientation, rotation1, rotation2, rotation3, spawntimesecs, animprogress, state) VALUES (175382, 229, 1, 1, 67.128304, -297.420990, 91.425499, -0.034906, 0, 0, 0, 300, 0, 1);
 DELETE FROM gameobject_questrelations WHERE quest = 4764;
-INSERT INTO gameobject_questrelation (id, quest) VALUES (NULL, 4764);
+INSERT INTO gameobject_questrelations (id, quest) VALUES (NULL, 4764);
 DELETE FROM gameobject_involvedrelation WHERE quest = 4764;
 INSERT INTO gameobject_involvedrelation (id, quest) VALUES (NULL, 4764);
 DELETE FROM gameobject_loot_template WHERE entry = 175382;
