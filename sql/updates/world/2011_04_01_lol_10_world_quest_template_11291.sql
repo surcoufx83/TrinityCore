efcc8aa6e75d0e0a9dd82e0cd2307b995353cae8
treak <stg90@gmx.de>
2011-04-04 11:29:19 +0200

    DB/Quests - Fix Quest 11023 Bomb them Again Reputation
    //revised

--- sql/updates/world/2011_04_01_lol_10_world_quest_template_11291.sql
+++ sql/updates/world/2011_04_01_lol_10_world_quest_template_11291.sql
@@ -14,6 +14,6 @@ DELETE FROM `smart_scripts` WHERE `entryorguid` = @Emilune;
 INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES 
 (@Emilune,0,0,0,62,0,100,0,@Gossip,0,0,0,11,@Spell,0x02,0,0,0,0,7,0,0,0,0,0,0,0,'on gossip select - cast taxi');
 -- conditions
-DELETE FROM `conditions` WHERE `SourceGroup` = @Gossip AND `SourceTypeOrReferenceId` = 28;
+DELETE FROM `conditions` WHERE `SourceGroup` = @Gossip AND `SourceTypeOrReferenceId` = 15;
 INSERT INTO `conditions` (`SourceTypeOrReferenceId`, `SourceGroup`, `SourceEntry`, `ElseGroup`, `ConditionTypeOrReference`, `ConditionValue1`, `ConditionValue2`, `ConditionValue3`, `ErrorTextId`, `ScriptName`, `Comment`) VALUES 
 (15,@Gossip,0,0,28,@Quest,0,0,0,'','show gossip menu option if player has completed 11291');
