41e574303fa7616933761cd61c77ae21036e1001
treak <stg90@gmx.de>
2011-05-20 22:22:59 +0200

    DB - correct 2011_5_14_0_lol_sum_world.sql

--- sql/old/lol/2011_5_14_0_lol_sum_world.sql
+++ sql/old/lol/2011_5_14_0_lol_sum_world.sql
@@ -19,12 +19,9 @@ INSERT INTO `npc_vendor` (`entry`,`slot`,`item`,`maxcount`,`incrtime`,`ExtendedC
 (35826,0,16054,1,1800,0),
 (35826,0,13311,1,1800,0),
 (35826,0,10609,1,1800,0);
--- change some data of Ability: Death and Decay (Deathknight)
--- source: http://old.wowhead.com/spell=43265
 DELETE FROM `spell_bonus_data` WHERE `entry`=52212;
 INSERT INTO `spell_bonus_data` (`entry`,`direct_bonus`,`dot_bonus`,`ap_bonus`,`ap_dot_bonus`,`comments`) VALUES 
-(52212,0,-1,0,0.064,'Death Knight - Death and Decay');
--- (52212,0,-1,0.0475,-1,'Death Knight - Death and Decay'); -- old 
+(52212,0,-1,0.0475,-1,'Death Knight - Death and Decay');
 DELETE FROM `spell_script_names` WHERE `spell_id` IN (62359,64979);
 INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (64979, 'spell_anti_air_rocket'),
