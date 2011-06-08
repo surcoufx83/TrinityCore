75d7602f77b070934abfc5fa65fb17dba534bc6d
Tarulia <takuja@gmx.de>
2011-06-08 22:43:30 +0200

    DB/Creatures: Add Passive Flag for some Ulduar Triggers

---
+++ sql/updates/world/world_creature_template.sql
@@ -0,0 +1,11 @@
+-- Add passive Flag to various Ulduar triggers
+UPDATE creature_template SET unit_flags = unit_flags|512 WHERE entry IN (
+33054, -- Thorim Trap Bunny
+33500, -- Vezax Bunny
+33575, -- Channel Stalker Freya
+33661, -- Armsweep Stalker Kologarn
+34188, -- Razorscale Devouring Flame Stalker
+34189, -- Razorscale Devouring Flame Stalker (heroic)
+34098, -- Auriaya Seeping Essence Stalker
+34174  -- Auriaya Seeping Essence Stalker (heroic)
+);
