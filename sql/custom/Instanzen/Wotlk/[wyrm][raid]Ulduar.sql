5b09f2f66d4fc5f36320f1062aecd43b754a316a
Tarulia <takuja@gmx.de>
2011-06-11 12:00:34 +0200

    DB/Creature: Add Last commit(s) to Ulduar summary

--- sql/custom/Instanzen/Wotlk/[wyrm][raid]Ulduar.sql
+++ sql/custom/Instanzen/Wotlk/[wyrm][raid]Ulduar.sql
@@ -648,3 +648,18 @@ INSERT INTO `spell_script_names` (`spell_id`,`ScriptName`) VALUES
 (64596, 'spell_cosmic_smash_dmg'),
 (64597, 'spell_cosmic_smash_missile_target'),
 (64445, 'spell_remove_player_from_phase');
+
+-- Add passive Flag to various Ulduar triggers
+UPDATE creature_template SET unit_flags = unit_flags|512 WHERE entry IN (
+33054, -- Thorim Trap Bunny
+33500, -- Vezax Bunny
+33575, -- Channel Stalker Freya
+33661, -- Armsweep Stalker Kologarn
+34188, -- Razorscale Devouring Flame Stalker
+34189, -- Razorscale Devouring Flame Stalker (heroic)
+34098, -- Auriaya Seeping Essence Stalker
+34174  -- Auriaya Seeping Essence Stalker (heroic)
+);
+
+-- Remove NON_SELECTABLE from Clockwork Mechanic
+UPDATE creature_template SET unit_flags = unit_flags &~ 33554432 WHERE entry = 34184;
