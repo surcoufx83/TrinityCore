c48a2df507507911fe51c58f0a332bc86aa6dbcd
treak <stg90@gmx.de>
2011-06-25 10:32:40 +0200

    DB/Loot - add Money to Ulduar Trash
    // correct code
    // add 25 man money

--- sql/updates/world/2011_06_24_lol_4_world_creature_template.sql
+++ sql/updates/world/2011_06_24_lol_4_world_creature_template.sql
@@ -1,14 +1,22 @@
 -- Fix Gold Loot of Ulduar
 SET @Gold_10_Trash_min  :=100000;
-SET @Gold_10_Trash_max  :=@Gold_10_Trash_min*1.2;
+SET @Gold_10_Trash_max  :=@Gold_10_Trash_min*1.5;
 SET @Gold_25_Trash_min  :=250000;
-SET @Gold_25_Trash_max  :=@Gold_25_Trash_min*1.2;
+SET @Gold_25_Trash_max  :=@Gold_25_Trash_min*1.5;
 
 UPDATE `creature_template` SET `mingold`=@Gold_10_Trash_min,`maxgold`=@Gold_10_Trash_max WHERE
-(SELECT*FROM `creature_template` WHERE 
+(SELECT entry FROM `creature_template` WHERE
 `entry` IN (SELECT `id` FROM `creature` WHERE `map`=603) AND    -- Ulduar Map
 `faction_A` IN (14,16) AND
-`faction_H` IN (14,16) AND 
-`rank`=1 AND 
+`faction_H` IN (14,16) AND
+`rank`=1 AND
+`entry` NOT IN (34234,33236,34164,34164,32872)                  -- exclude Fame Leviathane Trash
+);
+UPDATE `creature_template` SET `mingold`=@Gold_25_Trash_min,`maxgold`=@Gold_25_Trash_max WHERE
+(SELECT difficulty_entry_1 FROM `creature_template` WHERE
+`entry` IN (SELECT `id` FROM `creature` WHERE `map`=603) AND    -- Ulduar Map
+`faction_A` IN (14,16) AND
+`faction_H` IN (14,16) AND
+`rank`=1 AND
 `entry` NOT IN (34234,33236,34164,34164,32872)                  -- exclude Fame Leviathane Trash
 );
