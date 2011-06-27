8f2dfeb2a9d52ca00be33523d40d6ae878630260
treak <stg90@gmx.de>
2011-06-27 03:26:07 +0200

    DB/Loot - Fix Engineering Loot for Flame Leviathan, XT-002, Mimiron
    //add Mimiron to Engineer Loot
    //add some Items required for Engineering Mounts

--- sql/updates/world/2011_06_27_lol_0_world_skinning_loot_template.sql
+++ sql/updates/world/2011_06_27_lol_0_world_skinning_loot_template.sql
@@ -1,6 +1,6 @@
--- Fix Engineering Loot for Flame Leviathan & XT-002
+-- Fix Engineering Loot for Flame Leviathan, XT-002, Mimiron
 SET @ENTRY  :=100012;
-UPDATE `creature_template` SET `type_flags`=`type_flags`|32768,`skinloot`=@ENTRY WHERE `entry` IN (33113,34003,33293,33885);
+UPDATE `creature_template` SET `type_flags`=`type_flags`|32768,`skinloot`=@ENTRY WHERE `entry` IN (33113,34003,33293,33885,33350);
 DELETE FROM `skinning_loot_template` WHERE `entry` IN (@ENTRY);
 INSERT INTO `skinning_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`lootmode`,`groupid`,`mincountOrRef`,`maxcount`) VALUES
 (@ENTRY,35627,63,1,0,2,10), -- Eternal Shadow
@@ -14,4 +14,7 @@ INSERT INTO `skinning_loot_template` (`entry`,`item`,`ChanceOrQuestChance`,`loot
 (@ENTRY,39683,23,1,0,2,3),  -- Froststeel Tube
 (@ENTRY,39690,23,1,0,16,19),-- Volatile Blasting Trigger
 (@ENTRY,37663,14,1,0,1,2),  -- Titansteel Bar
-(@ENTRY,49050,5,1,0,1,1);   -- Schematic: Jeeves
+(@ENTRY,49050,10,1,0,1,1),  -- Schematic: Jeeves
+(@ENTRY,44499,3,1,0,1,1),   -- Salvaged Iron Golem Parts
+(@ENTRY,44501,3,1,0,2,4),   -- Goblin-machined Piston
+(@ENTRY,44500,3,1,0,1,1);   -- Elementium-plated Exhaust Pipe
