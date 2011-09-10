64dd9d2f3133edc5f7ef3f07ab854cff2b8e2267
BroodWyrm <sclare@web.de>
2011-09-10 09:54:31 +0200

    * Add summary SQLs

--- sql/updates/world/2011_09_06_lol_1_world_misc.sql
+++ sql/updates/world/2011_09_06_lol_1_world_misc.sql
@@ -1,2 +1,2 @@
-UPDATE `creature_template` SET `InhabityType`=7,`flags_extra`=`flags_extra`|0x10000000 WHERE `entry`=33687;
-UPDATE `creature` SET `spawntimesecs`=180 WHERE `entry`=33687;
+UPDATE `creature_template` SET `InhabitType`=7,`flags_extra`=`flags_extra`|0x10000000 WHERE `entry`=33687;
+UPDATE `creature` SET `spawntimesecs`=180 WHERE `id`=33687;
