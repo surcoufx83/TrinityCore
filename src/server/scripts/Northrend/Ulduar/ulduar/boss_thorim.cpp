surcoufx83/TrinityCore

commit e9d112e9d554a660b847842599d60e90c42baaeb

Scripts/Thorim: try to fix some problems due to bad home positioning

src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
@@ -291,6 +291,7 @@ public:
         {
             Wipe = false;
             EncounterFinished = false;
+            homePosition = creature->GetHomePosition();
         }
 
         Phases phase;
@@ -302,6 +303,7 @@ public:
         bool HardMode;
         bool OrbSummoned;
         bool EncounterFinished;
+        Position homePosition;
 
         void Reset()
         {
@@ -396,11 +398,28 @@ public:
                 go->RemoveFlag(GAMEOBJECT_FLAGS, GO_FLAG_NOT_SELECTABLE);
         }
 
+        void EnterEvadeMode()
+        {
+            if (!_EnterEvadeMode())
+                return;
+
+            me->SetHomePosition(homePosition);
+            me->GetMotionMaster()->MoveTargetedHome();
+            Reset();
+        }
+
         void UpdateAI(uint32 const diff)
         {
             if (!UpdateVictim())
                 return;
 
+            if (phase == PHASE_2 && me->getVictim() && !IN_ARENA(me->getVictim()))
+            {
+                me->getVictim()->getHostileRefManager().deleteReference(me);
+                return;
+            }
+
+            // still needed?
             if (phase == PHASE_2 && !IN_ARENA(me))
             {
                 EnterEvadeMode();
@@ -752,7 +771,10 @@ class npc_thorim_arena_phase : public CreatureScript
             void UpdateAI(uint32 const diff)
             {
                 if (me->getVictim() && !isOnSameSide(me->getVictim()))
+                {
                     me->getVictim()->getHostileRefManager().deleteReference(me);
+                    return;
+                }
 
                 if (!UpdateVictim() || me->HasUnitState(UNIT_STAT_CASTING))
                     return;

commit a9d486a93f5d19acdb71086e6b69cb37c76a31ed

Scripts/Thorim: try to fix problems introduced in https://github.com/darkman1983/TrinityCore/commit/e9d112e

src/server/scripts/Northrend/Ulduar/ulduar/boss_thorim.cpp
@@ -770,14 +770,14 @@ class npc_thorim_arena_phase : public CreatureScript
 
             void UpdateAI(uint32 const diff)
             {
+                if (!UpdateVictim() || me->HasUnitState(UNIT_STAT_CASTING))
+                    return;
+
                 if (me->getVictim() && !isOnSameSide(me->getVictim()))
                 {
                     me->getVictim()->getHostileRefManager().deleteReference(me);
                     return;
                 }
-
-                if (!UpdateVictim() || me->HasUnitState(UNIT_STAT_CASTING))
-                    return;
 
                 if (_PrimaryTimer <= diff)
                 {