c83d27ebfd272619ef0d2e1a03820c67e3ae79f3
treak <stg90@gmx.de>
2011-11-18 07:34:33 +0100

    Fix typos

--- sql/updates/world/2011_11_18_lol_0_world_spell_proc_event.sql
+++ sql/updates/world/2011_11_18_lol_0_world_spell_proc_event.sql
@@ -1,1 +1,1 @@
-UPDATE `spell_proc_event` SET `SpellFamillyMask0`=4096|8388608,`SpellFamillyMask1`=256,`SpellFamillyMask2`=2097152 WHERE `entry`=64752;
+UPDATE `spell_proc_event` SET `SpellFamilyMask0`=4096|8388608,`SpellFamilyMask1`=256,`SpellFamilyMask2`=2097152 WHERE `entry`=64752;
